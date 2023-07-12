a40a9b2dcb499646e11f81c5d4115cac7a489257
enik-ua <nik.ermolenko@gmail.com>
2023-07-12 18:43:54 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,96 +1,86 @@
-const hostname = '127.0.0.1';
-const port     = 3001;
-const acao     = "*";
-const database = "database";
-const http     = require('http');
-const sqlite   = require('sqlite3').verbose();
-const { v4: uuid } = require('uuid');
-const CryptoJS = require('crypto-js');
+const app    = require('./settings');
+const http   = require('http');
+const db     = require('sqlite3').verbose();
+const crypto = require('./engine/crypto');
 
-let db = new sqlite.Database(database,(err)=>{
+let blog = new db.Database(app.database,(err)=>{
     if (err) {
         return console.error(err.message);
     }
     console.log('Connected to the SQlite database.');
+    listenServer();
+    blog.close();
 });
-db.close();
 
 const server = http.createServer((query, packet) => {
     let data = "";
-
     query.on("data", (chunk) => {
       data += chunk;
     });
     query.on("end",()=>{
         packet.setHeader("Content-Type", "text/plain");
         packet.statusCode = 200;
-        packet.setHeader("Access-Control-Allow-Origin",acao);
+        packet.setHeader("Access-Control-Allow-Origin",app.acao);
 
-        db = new sqlite.Database('database');
+        blog = new db.Database(app.database);
         switch (query.method){
             case "GET":
                 switch (query.url){
                     case "/guid":
-                        packet.end(uuid());
+                        packet.end(0);
                         break;
                     case "/articles":
-                        db.serialize(()=>{
-                            db.all('SELECT * FROM articles;',(err,rows)=>{
+                        blog.serialize(()=>{
+                            blog.all('SELECT * FROM articles;',(err,rows)=>{
                                 if (err){
                                     console.error(err.message);
                                 }
                                 packet.end(JSON.stringify(rows));
+                                blog.close()
                             });
                         });
                         break;
                 }
                 break;
             case "POST":
+                data = JSON.parse(data);
                 switch (query.url){
                     case "/articles":
-                        let article = JSON.parse(data);
-                        db.serialize(()=>{
-                            let MD5 = CryptoJS.MD5(article.password);
-                            let hash = MD5.words[0]>0?MD5.words[0]:-MD5.words[0]+MD5.words[1]>0?MD5.words[1]:-MD5.words[1]+MD5.words[2]>0?MD5.words[2]:-MD5.words[2]+MD5.words[3]>0?MD5.words[3]:-MD5.words[3];
-                            db.get('SELECT login,password FROM users WHERE login="'+article.login+'" AND password="'+ hash+'";',[],(err,row)=>{
+                        blog.serialize(()=>{
+                            blog.get('SELECT login,password FROM users WHERE login=? AND password=?;',[data.login,crypto.MD5(data.password)],(err,row)=>{
                                 if(err){
                                     console.error(err.message);
                                 }else if(row==undefined){
                                     console.error("Помилка аутентифікації");
                                 }else{
-                                    db = new sqlite.Database('database');
-                                    db.run(`INSERT INTO articles (title,text,login) VALUES(?,?,?);`,[article.title,article.text,article.login],(err,row)=>{
+                                    blog.run(`INSERT INTO articles (title,text,login) VALUES(?,?,?);`,[data.title,data.text,data.login],(err,row)=>{
                                         if (err){
                                             console.error(err.message);
                                         }else{
                                             packet.end();
                                         }
+                                        blog.close();
                                     });
-                                    db.close();
                                 }
                             });
                         });
                         break;
                     case "/users":
-                        let user = JSON.parse(data);
-                        db.serialize(()=>{
-                    	    db.get('SELECT login FROM users WHERE login ="'+user.login+'";',[],(err,row)=>{
+                        blog.serialize(()=>{
+                    	    blog.get('SELECT login FROM users WHERE login = ?;',[data.login],(err,row)=>{
                                 if(err){
                     		        console.error(err.message);
                     		    }else if(row!==undefined){
                                     console.error("Логін використаний");
                                 }else{
-                                    db = new sqlite.Database('database');
-                                    let MD5 = CryptoJS.MD5(user.password);
-                                    let hash = MD5.words[0]>0?MD5.words[0]:-MD5.words[0]+MD5.words[1]>0?MD5.words[1]:-MD5.words[1]+MD5.words[2]>0?MD5.words[2]:-MD5.words[2]+MD5.words[3]>0?MD5.words[3]:-MD5.words[3];
-                                    db.run(`INSERT INTO users (login,password) VALUES(?,?);`,[user.login,hash],(err,row)=>{
+                                    blog.run(`INSERT INTO users (login,password) VALUES(?,?);`,[data.login,crypto.MD5(data.password)],(err,row)=>{
                                         if (err){
                                             console.error(err.message);
                                         }else{
                                             packet.end(row);
                                         }
+                                        blog.close();
                                     });
-                                    db.close();
                                 }
                     	    });
                         });            
@@ -98,18 +88,11 @@ const server = http.createServer((query, packet) => {
                 }
                 break;
         }
-        db.close();
     });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-function createResponse(query,packet){
-    packet.statusCode = 200;
-    packet.setHeader('Content-Type', 'text/plain');
-    packet.end('Hello World');
-    console.log(`Connect to server at http://${hostname}:${port}/`);
-    console.log(packet);
+function listenServer(){
+    server.listen(app.port,app.hostname,undefined,()=>{
+        console.log(`Server running at http://${app.hostname}:${app.port}/`);
+    })
 }
